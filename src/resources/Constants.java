457b4085ac581662c651ff0dc378a8b024819df6
Chetna <chetna@techpertitsolutions.com>
2020-04-08 02:49:12 +0530

    Empty Project

--- src/resources/Constants.java
+++
@@ -1,9 +0,0 @@
-//user defined package
-package resources;
-
-public class Constants {
-	public static final String NO = "NO";
-	public static final String YES = "YES";
-	public static final String REGULAR = "REGULAR";
-	public static final String CORPORATE = "CORPORATE";
-}
