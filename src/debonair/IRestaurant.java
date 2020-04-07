68011c47b3166f418faffa6cf1c47a37b054e89b
Neeraj jain (OSV) <neeraj.jain.osv@fedex.com>
2020-04-07 16:20:45 -0500

    Deleted code from master

--- src/debonair/IRestaurant.java
+++
@@ -1,9 +0,0 @@
-package debonair;
-
-public interface IRestaurant {
-	public void showMenu();
-	public void takeOrder();
-	public boolean checkStatus(int orderId);
-	public void showRegularCustomerOffers(int custId);
-	public void showCorporateCustomerOffers(int corpId);
-}
